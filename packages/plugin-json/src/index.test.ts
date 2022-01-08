64a97f077628a7427c7ea92edcd5dbd947f409a3
Theophile Avoyne <tavoyne@gmail.com>
2022-01-08 12:03:50 +0100

    WIP babel-webpack-typescriptIZING (plugin-json)

--- packages/plugin-json/src/index.test.ts
+++ packages/plugin-json/src/index.test.ts
@@ -1,36 +1,35 @@
-"use strict";
+import plugin from "eslint-plugin-jsonc";
+// @ts-ignore
+import { pick } from "lodash";
 
-const assert = require("assert");
-const plugin = require("eslint-plugin-jsonc");
-const { pick } = require("lodash");
+import all from "./configs/all";
 
 const allRules = plugin.rules;
 const conflictingRules = plugin.configs.base.overrides[0].rules;
 const forbiddenRules = plugin.configs.prettier.rules;
 
-const providedRules = require("./index").configs.all.overrides[0].rules;
+const providedRules = all.overrides[0].rules;
 
 const allRulesKeys = Object.keys(allRules)
   .map((name) => {
     return `jsonc/${name}`;
   })
   .filter((name) => {
+    // @ts-ignore
     return !forbiddenRules[name];
   })
   .sort();
 
 const providedRulesKeys = Object.keys(providedRules)
   .filter((name) => {
+    // @ts-ignore
     return !conflictingRules[name];
   })
   .sort();
 
-describe("JSON:", () => {
-  it("All rules are present.", () => {
-    assert.deepEqual(
-      pick(providedRules, Object.keys(conflictingRules)),
-      conflictingRules
-    );
-    assert.deepEqual(allRulesKeys, providedRulesKeys);
-  });
+test("All rules are present.", () => {
+  expect(pick(providedRules, Object.keys(conflictingRules))).toEqual(
+    conflictingRules
+  );
+  expect(allRulesKeys).toEqual(providedRulesKeys);
 });
