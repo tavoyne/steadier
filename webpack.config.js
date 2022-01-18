22fa08d1c817ab8a9ebefe9b0cdb02aba4155963
Theophile Avoyne <tavoyne@gmail.com>
2022-01-18 16:27:14 +0100

    Set deps and peerDeps as externals (webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,26 @@
 
 const path = require("path");
 
+const manifest = require(path.join(
+  // eslint-disable-next-line node/no-process-env
+  process.env.INIT_CWD,
+  "package.json"
+));
+
+const externals = {
+  ...manifest.dependencies,
+  ...manifest.peerDependencies,
+};
+
+Object.keys(externals).forEach((key) => {
+  externals[key] = key;
+});
+
 module.exports = {
   // eslint-disable-next-line node/no-process-env
   entry: path.join(process.env.INIT_CWD, "src", "index.ts"),
+  externals,
+  externalsPresets: { node: true },
   mode: "production",
   module: {
     rules: [
